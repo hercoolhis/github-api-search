9dba5d54e258d989dc69493948ce5e52d0f21658
hercoolhis <agbenla.lanre@gmail.com>
2023-06-05 13:56:31 +0100

    test checks 25

--- src/components/ui/PaginationButton.js
+++ src/components/ui/PaginationButton.js
@@ -10,6 +10,7 @@ export default function PaginationButton({
   let disabledStyle =
     "inline-flex items-center cursor-not-allowed disabled disabled:opacity-25 px-4 py-2 mr-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg";
   let validStyle =
+  
     "inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white";
 
   return (
