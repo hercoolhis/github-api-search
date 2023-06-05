19bfb2da457643f7eb52eccbb7c69b161fd87c29
hercoolhis <agbenla.lanre@gmail.com>
2023-06-05 11:30:19 +0100

    test checks 15

--- src/components/ui/SearchBar.js
+++ src/components/ui/SearchBar.js
@@ -14,9 +14,14 @@ export default function SearchBar({
           className="mb-2 mt-4 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
           Search Repos By Names filter Musks boys jnjwdbw ndjnj
+
+
+
         </label>
-        <div className="relative">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <div className="relative"><div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+
+
+
             <svg
               aria-hidden="true"
               className="w-5 h-5 text-gray-500 dark:text-gray-400"
@@ -25,12 +30,12 @@ export default function SearchBar({
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              ></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
             </svg>
           </div>
           <input
