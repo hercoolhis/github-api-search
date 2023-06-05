b2c74cadd789edb3530d6ef39aafbd11cdccea08
hercoolhis <agbenla.lanre@gmail.com>
2023-06-05 14:08:54 +0100

    test checks 28

--- src/components/ui/Pagination.js
+++ src/components/ui/Pagination.js
@@ -41,6 +41,8 @@ const BackIcon = () => {
         d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
         clipRule="evenodd"
       ></path>
+
+      
     </svg>
   );
 };
@@ -51,7 +53,7 @@ export default function Pagination({ paginateInfo, getRepos, query }) {
   useEffect(() => {
     if (paginateInfo) {
 
-      
+
       setPaginatePoints(resolvePaginationData(paginateInfo));
     }
   }, [paginateInfo]);
--- src/components/ui/SearchBar.js
+++ src/components/ui/SearchBar.js
@@ -17,8 +17,6 @@ export default function SearchBar({
 
 
 
-
-
         </label>
         <div className="relative"><div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
 
