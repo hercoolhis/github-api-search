c1fb54aef7604aabaf9126994413017ce4c0e512
hercoolhis <agbenla.lanre@gmail.com>
2023-06-26 07:19:09 +0100

    test this linting again 2

--- src/components/ui/SearchBar.js
+++ src/components/ui/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function SearchBar({
+export default  function SearchBar({
   placeholder,
   text,
   submitHandler,
@@ -15,15 +15,10 @@ export default function SearchBar({
           htmlFor="default-search"
           className="mb-2 mt-4 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
-<<<<<<< HEAD
-
-          Search everything
-=======
           Search Repos By Names filter Musks boys jnjwdbw ndjnj jbdjbdj hdbhd bjdbdd vhvhdvhvd kjnajs
 
 
 
->>>>>>> ab5e21eb9cf4628e92e2ddbf9ed1fed83250b7dd
         </label>
         <div className="relative"><div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
 
