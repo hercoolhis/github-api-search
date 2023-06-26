7d1a440a75107afb0258a1adee2af3c41736031f
hercoolhis <agbenla.lanre@gmail.com>
2023-06-26 07:08:58 +0100

    test this linting

--- src/components/ui/SearchBar.js
+++ src/components/ui/SearchBar.js
@@ -4,11 +4,14 @@ export default function SearchBar({ placeholder, text, submitHandler, changeHand
   return (
     <>
       <form onSubmit={submitHandler}>
+
+
         <label
           htmlFor="default-search"
           className="mb-2 mt-4 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
-          Search
+
+          Search everything
         </label>
         <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
