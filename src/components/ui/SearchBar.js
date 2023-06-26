e91bbfd422b9e9f9c04555cae42585ab59ac1619
hercoolhis <agbenla.lanre@gmail.com>
2023-06-26 07:31:29 +0100

    fix search bar

--- src/components/ui/SearchBar.js
+++ src/components/ui/SearchBar.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-export default  function SearchBar({
-  placeholder,
+export default function SearchBar({placeholder,
   text,
   submitHandler,
-  changeHandler,
-}) {
+  changeHandler,}) {
   return (
     <>
       <form onSubmit={submitHandler}>
-
 
         <label
           htmlFor="default-search"
@@ -17,45 +14,41 @@ export default  function SearchBar({
         >
           Search Repos By Names filter Musks boys jnjwdbw ndjnj jbdjbdj hdbhd bjdbdd vhvhdvhvd kjnajs
 
-
-
         </label>
         <div className="relative"><div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
 
-
-
-            <svg
-              aria-hidden="true"
-              className="w-5 h-5 text-gray-500 dark:text-gray-400"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-            </svg>
-          </div>
-          <input
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </div>
+        <input
             type="search"
             id="default-search"
             className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder={placeholder}
             onChange={(e) => {
-              changeHandler(e.target.value);
-            }}
+            changeHandler(e.target.value);
+          }}
             value={text}
             required
           />
-          <button
+        <button
             type="submit"
             className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Search
+          Search
           </button>
         </div>
       </form>
